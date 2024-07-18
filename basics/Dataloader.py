25964f8e4e897d30b9bbdfa740ed6aaf46fb4451
aiko77777 <139691979+aiko77777@users.noreply.github.com>
2024-07-19 00:20:41 +0800

    Update Dataloader.py

--- basics/Dataloader.py
+++ basics/Dataloader.py
@@ -6,11 +6,10 @@ test_data = torchvision.datasets.CIFAR10("./dataset",train=False,transform=torch
 img, target = test_data[0]
 print(img.shape)
 print(img)
-
-# batch_size=4 使得 img0, target0 = dataset[0]、img1, target1 = dataset[1]、img2, target2 = dataset[2]、img3, target3 = dataset[3]，然后这四个数据作为Dataloader的一个返回      
+#load data
 test_loader = DataLoader(dataset=test_data,batch_size=4,shuffle=True,num_workers=0,drop_last=False)      
 
 for data in test_loader:
-    imgs, targets = data            
+    imgs, targets = data   #output [4,3,32,32]  
     print(imgs.shape)
     print(targets)
